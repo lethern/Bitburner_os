2cf04e7e10cc5b035a453b07e486bbdab61a7a4f
lethern <lethern@gmail.com>
2022-01-01 20:35:18 +0100

    x

--- install.js
+++ install.js
@@ -4,7 +4,7 @@ let json_filename = 'install_files_json.txt';
 /** @param {NS} ns */
 export async function main(ns) {
 	let { welcomeLabel, filesToDownload } = await fetchConfig(ns)
-	
+
 	ns.tprintf("%s", welcomeLabel)
 
 	let hostname = ns.getHostname()
@@ -18,18 +18,18 @@ export async function main(ns) {
 	let count = 0;
 	for (let filename of filesToDownload) {
 		const path = baseUrl + filename
-		const save_filename = (!filename.startsWith('/') && filename.includes('/'))? '/' + filename : filename;
-		
-		try{
+		const save_filename = (!filename.startsWith('/') && filename.includes('/')) ? '/' + filename : filename;
+
+		try {
 			await ns.scriptKill(save_filename, 'home')
 			await ns.rm(save_filename)
 			await ns.sleep(20)
 			await ns.wget(path + '?ts=' + new Date().getTime(), save_filename)
-			
-			if(++count % 5 ==0){
-				ns.tprintf(`Installing... [${(count+'').padStart(2)}/${filesToDownload.length}]`);
+
+			if (++count % 5 == 0) {
+				ns.tprintf(`Installing... [${(count + '').padStart(2)}/${filesToDownload.length}]`);
 			}
-		}catch(e){
+		} catch (e) {
 			ns.tprint(`ERROR (tried to download  ${path})`)
 			throw e;
 		}
@@ -44,13 +44,13 @@ async function clean(ns, filesToDownload) {
 	let filesRaw = filesToDownload.map(file => file.substr(file.lastIndexOf('/') + 1))
 	let allFiles = ns.ls("home");
 	let toDelete = [];
-	allFiles.forEach(file => {
-		if (file.startsWith('/')) file = file.substr(1);
+	allFiles.forEach(_file => {
+		let file = (_file.startsWith('/')) ? _file.substr(1) : _file;
 
-		if (file.startsWith('os/')){
+		if (file.startsWith('os/')) {
 			let file_raw = file.substr(file.lastIndexOf('/') + 1);
 			if (filesRaw.includes(file_raw) && !filesToDownload.includes(file)) {
-				toDelete.push(file);
+				toDelete.push(_file);
 			}
 		}
 	})
@@ -68,17 +68,17 @@ async function fetchConfig(ns) {
 		await ns.rm(local_filename)
 		await ns.wget(baseUrl + json_filename + '?ts=' + new Date().getTime(), local_filename)
 		return JSON.parse(ns.read(local_filename));
-	}catch(e){
+	} catch (e) {
 		ns.tprint(`ERROR: Downloading and reading config file failed ${json_filename}`);
 		throw e;
 	}
 }
 
-function terminalCommand(message){
+function terminalCommand(message) {
 	const docs = globalThis['document']
 	const terminalInput = /** @type {HTMLInputElement} */ (docs.getElementById("terminal-input"));
-	terminalInput.value=message;
+	terminalInput.value = message;
 	const handler = Object.keys(terminalInput)[1];
-	terminalInput[handler].onChange({target:terminalInput});
-	terminalInput[handler].onKeyDown({keyCode:13,preventDefault:()=>null});
+	terminalInput[handler].onChange({ target: terminalInput });
+	terminalInput[handler].onKeyDown({ keyCode: 13, preventDefault: () => null });
 }
